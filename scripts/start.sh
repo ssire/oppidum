9e5a5d61391b378f7bdf1e06189da4ce3990c5dc
Sire <s.sire@free.fr>
2011-12-02 10:26:45 +0100

    Set EXIST_HOME in start/stop script since this required at least since eXist 1.4.1-rev15155

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,2 +1,6 @@
+path=`pwd`
+EXIST_HOME=${path%%/webapp*scripts}; 
+export EXIST_HOME
 echo "Starting eXist server with output redirected to 'server.log'"       
-../../../../bin/startup.sh > server.log &  
+echo "running with EXIST_HOME set to $EXIST_HOME"
+../../../../bin/startup.sh > server.log &
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,2 +1,6 @@
-echo "Shutting down eXist server"       
-../../../../bin/shutdown.sh -u admin -p test
+password=test
+path=`pwd`
+EXIST_HOME=${path%%/webapp*scripts}; 
+export EXIST_HOME
+echo "Shutting down eXist server with EXIST_HOME=$EXIST_HOME and password $password"       
+../../../../bin/shutdown.sh -u admin -p $password
