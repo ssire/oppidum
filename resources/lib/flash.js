340b703c72ed3b3acaecfd39fa67a1c86722118e
Sire <s.sire@free.fr>
2013-07-15 17:39:56 +0200

    Added support for data-timeout="dur" where dur is a duration in ms before discarding the message on the message div (note that it works iff first span in message div is the data-timeout holder).

--- resources/lib/flash.js
+++ resources/lib/flash.js
@@ -1,11 +1,27 @@
-(function () { 
-  
+(function () {
+
+  function init() {
+    var n = document.getElementById('message'),
+        b = n ? n.getElementsByTagName('span').item(0) : null, // supposes 1st is close button
+        to, dur;
+    if (b) {
+      to = b.getAttribute('data-timeout');
+      if (to) {
+        dur = parseInt(to, 10);
+        if (!isNaN(dur)) {
+          setTimeout(document.messageOff, dur);
+        }
+      }
+    }
+  }
+
   function discard ( name ) {
     var n = document.getElementById(name);
     n.style.display = 'none';
   }
-    
+
   document.messageOff = function () { discard('message') };
   document.errorOff = function () { discard('error') };
-  
-})();
+
+  window.onload = init;
+}());
