6f397d4b50bdcf9db0998391b5eb2b9b9679f689
Stéphane <s.sire@oppidoc.fr>
2026-09-22 11:06:20 +0200

    [SMEIMKT-10063] Added a password to the docker bootstrap script (this imposes to configure an admin password on the database first)

--- scripts/bootstrap-docker.sh
+++ scripts/bootstrap-docker.sh
@@ -13,15 +13,15 @@ if ([ $# -lt 2 ]); then
 fi
 
 echo "loading oppidum mesh collection"
-docker exec $1 java org.exist.start.Main client -u admin -m "/db/www/oppidum/mesh" -s
-docker exec $1 java org.exist.start.Main client -u admin --collection "/db/www/oppidum/mesh" --parse "/exist/etc/webapp/projects/oppidum/mesh" -s
+docker exec $1 java org.exist.start.Main client -u admin -P $2 -m "/db/www/oppidum/mesh" -s
+docker exec $1 java org.exist.start.Main client -u admin -P $2 --collection "/db/www/oppidum/mesh" --parse "/exist/etc/webapp/projects/oppidum/mesh" -s
 
 echo "loading oppidum config collection"
-docker exec $1 java org.exist.start.Main client -u admin -m "/db/www/oppidum/config" -s
-docker exec $1 java org.exist.start.Main client -u admin --collection "/db/www/oppidum/config" --parse "/exist/etc/webapp/projects/oppidum/init" -s
+docker exec $1 java org.exist.start.Main client -u admin -P $2 -m "/db/www/oppidum/config" -s
+docker exec $1 java org.exist.start.Main client -u admin -P $2 --collection "/db/www/oppidum/config" --parse "/exist/etc/webapp/projects/oppidum/init" -s
 
 echo "configuring oppidum permissions on collections"
-docker exec $1 java org.exist.start.Main client -u admin -F "/exist/etc/webapp/projects/oppidum/scripts/bootstrap.xql"
+docker exec $1 java org.exist.start.Main client -u admin -P $2 -F "/exist/etc/webapp/projects/oppidum/scripts/bootstrap.xql"
 
 # uncomment next 2 lines if you want to debug oppidum from eXide
 # docker exec $1 java org.exist.start.Main client -u admin -m "/db/www/oppidum/lib" -s
